6bea16e6910d9a1949513800f6cdc3f96ca31f9b
Vishalag74 <vishalagrawal7037@gmail.com>
2025-06-09 23:54:01 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
 import EmpDashboard from './components/Dashboard/EmpDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
-import { getLocalStorage, setLocalStorage } from './utils/LocalStorage'
-import { useState } from 'react'
+import { AuthContext } from './context/AuthProvider'
 
 const App = () => {
 
   const [user, setUser] = useState(null)
+  const authData = useContext(AuthContext)
+
 
   const handleLogin = (email, password) => {
     if (email === "admin@me.com" && password === "123") {
       setUser("admin");
-      console.log(user)
     } else if (email === "user@me.com" && password === "123") {
       setUser("employee");
-      console.log(user)
-    }
-    else {
+    } else {
       alert("Invalid credentials, please try again.");
     }
   }
-
 
   return (
     <>
